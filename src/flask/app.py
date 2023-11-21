b3f21e7e9d682e88a0c565e4b53f323dd3caf2c5
Cathal Ryan <cathal.ryan247@mail.dcu.ie>
2023-11-21 11:26:28 +0000

    Update app.py

--- src/flask/app.py
+++ src/flask/app.py
@@ -10,13 +10,10 @@ def index():
     ''' home page of app '''
     return render_template('index.html')
 
-@app.route('/predict')
+@app.route('/predict',methods=['POST'])
 def predict():
-    ''' load a model and vocab and apply it to a sample review '''
-    model = pickle.load(open('ca337-nb1000-model.pkl','rb'))
-    features = pickle.load(open('ca337-nb1000-features.pkl','rb'))
-    review = 'I love it'
-    prediction = model.predict(features.transform([review]))[0]
-    return render_template('predict.html',result={'text':review,'prediction':prediction})
-
-app.run(host='0.0.0.0', port=5000)
+ model = pickle.load(open('ca337-nb1000-model.pkl','rb'))
+ features = pickle.load(open('ca337-nb1000-features.pkl','rb')
+ review = request.form['review']
+ prediction = model.predict(features.transform([review]))[0]
+ return render_template('predict.html',result={'text':review,'prediction':prediction})
