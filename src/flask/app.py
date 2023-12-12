92ca52c2850ae38d3541da4e3c99d75e0dc8793d
Cathal Ryan <cathal.ryan247@mail.dcu.ie>
2023-12-12 14:25:05 +0000

    Update app.py

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
@@ -10,10 +9,15 @@ def index():
     ''' home page of app '''
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
- model = pickle.load(open('ca337-nb1000-model.pkl','rb'))
- features = pickle.load(open('ca337-nb1000-features.pkl','rb')
- review = request.form(['review'])
- prediction = model.predict(features.transform([review]))[0]
- return render_template('predict.html',result={'text':review,'prediction':prediction})
+    model = pickle.load(open('ca337-nb1000-model.pkl', 'rb'))
+    features = pickle.load(open('ca337-nb1000-features.pkl', 'rb'))
+    
+    review = request.form['review']
+    prediction = model.predict(features.transform([review]))[0]
+    
+    return render_template('predict.html', result={'text': review, 'prediction': prediction})
+
+if __name__ == '__main__':
+    app.run(debug=True)
